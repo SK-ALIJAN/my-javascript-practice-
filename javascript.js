938f1372c67a8268ab82198b43450d1a58d29dc8
SK.ALIJAN <alizaan8695@gmail.com>
2022-08-19 08:52:55 +0530

    this is nothing but only my javascript practice

---
+++ javascript.js
@@ -0,0 +1,155 @@
+console.log("write your code here")
+
+// ternary operater in javacsript
+
+/*
+if (voter_age>=19){
+console.log("you are  eligible for voting")
+}
+else if (voter_age==18) {
+    console.log("please come here for voting")
+}
+
+else{
+console.log(
+"you are not eligible for this becouse you are under 18 ")
+}*/
+
+//console.log(` ${voter_age19} `)
+
+/*switch (new Date().getDay()) {
+ case 0:
+        day="sunday"
+        break;
+ case 1:
+        day="monday"
+        break;
+ case 2:
+        day="tuesday"
+        break;
+
+ case 3:
+        day="wednesday"
+        break;
+ case 4:
+        day="thursday"
+        break;
+
+ case 5:
+        day="friday"
+        break;
+   case 6:
+        day="satureday"
+        break;
+      
+
+    default: //console.log("here this is for vote varify")
+        break;
+}
+console.log(day)*/
+
+/*var month;
+switch (new Date().getMonth()) {
+       case 0:
+              month="january"
+              break;
+              
+case 1:
+              month="february"
+              break;
+case 2:
+              month="march"
+              break;
+case 3:
+              month="april"
+              break;
+case 4:
+              month="may"
+              break;
+case 5:
+              month="june"
+              break;
+case 6:
+              month="jully"
+              break;
+case 7:
+              month="august"
+              break;
+case 8:
+              month="september"
+              break;
+case 9:
+              month="october"
+              break;
+case 10:
+              month="november"
+              break;
+case 11:
+              month=december
+              break;
+
+
+       default:month="not any month"
+              break;
+}
+console.log(month)*/
+
+/*let ask=prompt("what is your state name ?? please enter here")
+
+let obj={
+name:"alijan",
+city:"kolkata",
+state:"west bengal",
+pin: 721601
+
+}
+function practice(){
+if (ask==obj.state){
+     return ("congrazes! you are bengali too ")  
+}
+else {
+return("sorry");
+}
+}*/
+
+/*var number =20;
+var numbers=40;
+
+       function num1(day,month,year) {
+setTimeout(() => {alert("bhai alijan ! "+year + " hogaya hai")
+       
+},2000); 
+
+setTimeout(() => {alert(month +" month is now")
+       
+},3000); 
+
+setTimeout(() => {alert("hey alijan , what are you doning now its " +day +" date of this month hurry up bhai hurry up")
+       
+}, 4000);
+              function num2() {
+             return(number)
+
+              }
+              
+               return num2();
+       }
+/*function num5() {
+              function num6() {
+             
+
+                 function nested_num24() {
+                   return(numbers);  
+              }
+               return nested_num24();
+              }
+              
+              //return num6()
+               return num6();
+       }*/
+
+
+
+
+
+
